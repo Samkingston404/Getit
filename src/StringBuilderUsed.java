da4e7b2f6a80ab96791a3547be45b86614c39629
Samkingston404 <rsamuelvivek900@gamil.com>
2025-01-25 22:38:08 +0530

    The longest word in the sentence printed.

---
+++ src/StringBuilderUsed.java
@@ -0,0 +1,7 @@
+public class StringBuilderUsed {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("Samuel");
+        str.append(" Kingston");
+        System.out.println(str);
+    }
+}
